3b66bfb16e4266bb5bd5d2c42e3ea62ad586bf21
Kiran Namawar <kirannamawar@gmail.com>
2025-02-21 23:29:07 +0530

    reverse notes order in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import prisma from "./util/prisma";
 
 export default async function Home() {
   await connection()
-  const notes = await prisma.note.findMany()
+  const notes = (await prisma.note.findMany()).toReversed()
   return (
     <main className="m-1 min-h-screen">
       <h1 className="text-4xl text-red-500 text-center m-4 font-bold">Red Notes</h1>
